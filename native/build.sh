ba21aae756e65f50c1f51342b5b62da2afa16997
xenoamess <xenoamess@gmail.com>
2026-07-16 00:15:48 +0800

    perf: switch to Clang 22.1.8 + ThinLTO on custom centos7-toolchain container
    - build.yml: use ghcr.io/xenoamess/centos7-toolchain:clang22-lto
      (Clang 22.1.8 + lld linker for ThinLTO support)
    - build.sh: add -flto=thin -fuse-ld=lld to x86_64 CFLAGS/CXXFLAGS
      (retain -funroll-loops -fomit-frame-pointer)

--- native/build.sh
+++ native/build.sh
@@ -120,8 +120,10 @@ cmake -DCMAKE_BUILD_TYPE=Release \
         -DBUILD_BENCHMARKS=false \
         -DBUILD_EXAMPLES=false \
         -DBUILD_TOOLS=false \
-        -DCMAKE_C_FLAGS="-march=$MARCH -funroll-loops -fomit-frame-pointer" \
-        -DCMAKE_CXX_FLAGS="-march=$MARCH -funroll-loops -fomit-frame-pointer" \
+        -DCMAKE_C_FLAGS="-march=$MARCH -funroll-loops -fomit-frame-pointer -flto=thin" \
+        -DCMAKE_CXX_FLAGS="-march=$MARCH -funroll-loops -fomit-frame-pointer -flto=thin" \
+        -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld" \
+        -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld" \
         .
   make -j $THREADS install/strip
   ;;
